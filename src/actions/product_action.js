6715b5f84f83c1c205d7f1417d6d2db86294f620
hellomac87 <hellomac87@gmail.com>
2019-02-25 16:59:47 +0900

    feat: refact product action

--- src/actions/product_action.js
+++ src/actions/product_action.js
@@ -4,15 +4,25 @@ import { SET_PRODUCT } from "../reducers/product_reducer";
 
 export const getProduct = category => async disaptch => {
   const params = {
-    category: category ? category : null
+    category: category || null
   };
   try {
+    // Start. 로딩 인디케이터 시작
     disaptch(loadingStart());
+
+    // 1. call api
     const res = await api.get("/products", { params });
     const payload = res.data;
+
+    // 2. dispatch setProduct action
     disaptch(setProduct(payload, category));
+  } catch (e) {
+    // Error
+    console.log(e);
+  } finally {
+    // END. 로딩 인디케이터 끝
     disaptch(loadingEnd());
-  } catch (e) {}
+  }
 };
 
 const setProduct = (payload, category) => {
